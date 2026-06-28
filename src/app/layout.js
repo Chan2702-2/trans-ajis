8972c3a1619ba8dcfc87cf93d7fd9acd0c34d520
lad_polres <arifchan999@gmail.com>
2026-06-28 15:08:59 +0700

    update: search console dan SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,16 +13,20 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Fajri Tour & Travel Batam",
-  description: "Pilihan paket tour Batam & Bintan terlengkap dengan armada prima dan fasilitas premium. Dapatkan promo bonus beli 10 pax gratis 1!",
+  // Gw tambahin kata 'Pakej', 'Singapore', dan 'Malaysia' biar robot Google SG/MY gampang nangkep
+  description: "Pilihan paket tour & pakej percutian Batam - Bintan terlengkap untuk jemaah lokal, Singapore, & Malaysia. Promo bonus beli 10 pax gratis 1!",
   icons: {
     icon: "/asset/img/logo-1.png",
+  },
+  verification: {
+    google: "4Fy39JwJArDnY-szTJvLq47EZOFU33L4kiPlAZ_8Krk",
   }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
+      lang="id" // Diubah ke 'id' agar sesuai dengan bahasa utama web
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
